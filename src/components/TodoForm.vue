1ce9e35b0a9f157d947697977d17f374ab3f05b6
Xun Zhou <xun.zhou@lidl.com>
2019-03-12 19:10:18 +0100

    add new component

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <input class="new-todo"
+         autofocus autocomplete="off"
+         placeholder="What needs to be done?"
+         v-model="newTodo"
+         @keyup.enter="keyupEnterHandler(newTodo)">
+</template>
+
+<script>
+  export default {
+    name: "TodoForm",
+    props: ['addNewTodo'],
+    data () {
+      return {
+        newTodo: '',
+      }
+    },
+    methods: {
+      keyupEnterHandler(todo) {
+        this.$parent.addNewTodo(todo)
+        this.newTodo = ''
+      }
+    },
+  };
+</script>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <li class="todo" :class="{ completed: todo.completed, editing: todo == editedTodo }">
+    <div class="view">
+      <input class="toggle" type="checkbox" v-model="todo.completed">
+      <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
+      <button class="destroy" @click="deleteClickHandler(todo)"></button>
+    </div>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: "TodoItem",
+    props: ['todo'],
+    data() {
+      return {
+        editedTodo: null,
+      }
+    },
+    methods: {
+      deleteClickHandler(todo) {
+        this.$parent.removeTodo(todo)
+      },
+      editTodo() {},
+      doneEdit() {},
+      cancelEdit() {},
+    }
+  };
+</script>
