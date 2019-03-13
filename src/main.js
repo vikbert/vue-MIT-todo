6542fac2733d6986bb5ab40269de79be2e327bd7
Xun Zhou <xun.zhou@lidl.com>
2019-03-13 02:03:53 +0100

    refactor the local storage implementation to main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import TodoApp from './TodoApp.vue'
 
-Vue.config.productionTip = false
-
 const STORAGE_KEY = 'VUE_APP_TODO_LIST';
-const todoStorage = {
+const todoLocalStorage = {
   uid: 0,
   fetch: function () {
     let todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
-    todoStorage.uid = todos.length;
+    todoLocalStorage.uid = todos.length;
     todos.forEach(function (todo, index) {
       todo.id = index;
     });
@@ -20,7 +18,8 @@ const todoStorage = {
   },
 };
 
-Vue.prototype.$storage = todoStorage
+Vue.config.productionTip = false
+Vue.prototype.$storage = todoLocalStorage
 
 new Vue({
   render: h => h(TodoApp),
